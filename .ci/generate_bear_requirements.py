7e3452a9d45a14f7d3740e2a28535ee7e94bba2c
John Vandenberg <jayvdb@gmail.com>
2018-08-17 00:53:06 +0700

    generate_bear_requirementsTest.py: Add

--- .ci/generate_bear_requirements.py
+++ .ci/generate_bear_requirements.py
@@ -61,7 +61,7 @@ INSTANCE_NAMES = (
 )
 
 
-def get_args():
+def get_args(args=None):
     parser = argparse.ArgumentParser(
         description='This program generates a yaml requirement file for '
                     'installation of linters that are used by the bears.')
@@ -77,7 +77,7 @@ def get_args():
     parser.add_argument('--update', '-u', action='store_true',
                         help='updates "bear-requirements.yaml" '
                              'instead of overwriting')
-    args = parser.parse_args()
+    args = parser.parse_args(args=args)
 
     return args
 
@@ -214,8 +214,8 @@ def sort_requirements(req_dict):
                                             key=lambda t: t[0]))
 
 
-if __name__ == '__main__':
-    args = get_args()
+def main(args):
+    args = get_args(args)
 
     bear_dirs = [PROJECT_BEAR_DIR]
 
@@ -275,3 +275,7 @@ if __name__ == '__main__':
     sort_requirements(requirements)
     yaml.dump(requirements, output)
     output.close()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
